a3b0e445ea6282f5b78300a073ca207385240cc3
Veerali Shah <veerali.shah@drcsystems.com>
2017-07-03 11:21:32 +0530

    code change

--- config.py
+++ config.py
@@ -1,21 +1,35 @@
 #-*- coding: utf8 -*-
 
 import MySQLdb as mdb
+import warnings
 
 # CREATE THE DATABASE
 
-db = mdb.connect(host="localhost", user="USERNAME", passwd="PASSWORD")
-db1 = db.cursor()
-db1.execute('CREATE DATABASE dbname')
+
+def createdb_mysql():
+    db = mdb.connect(host="localhost", user="root", passwd="Drc@1234")
+    db1 = db.cursor()
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        db1.execute('CREATE DATABASE IF NOT EXISTS testdb')
+        print('Create Database testdb')
 
 
 # SET UP THE CONNECTION
 
+
 try:
-    con = mdb.connect('localhost', 'USERNAME', 'PASSWORD', 'DATABASE')
+    con = mdb.connect('localhost', 'root', 'Drc@1234', 'testdb')
     cur = con.cursor()
+    cur.execute("SELECT VERSION()")
+    ver = cur.fetchone()
+    print "Database version : %s " % ver
+
 
 except mdb.Error, e:
 
     print "Error %d: %s" % (e.args[0], e.args[1])
     sys.exit(1)
+
+
+createdb_mysql()
